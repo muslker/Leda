4366e3aefedc9863deb1d03e08281bb17b3fcf90
Mustafa Ülker <48808788+mustafaulker@users.noreply.github.com>
2020-10-07 16:06:58 +0300

    Project documents added.
    Database Scripts, ER Diagram, UML Diagrams, Software Report added.

---
+++ docs/Database/tbl_part-script.sql
@@ -0,0 +1,9 @@
+create table tbl_part
+(
+    part_id INTEGER
+        primary key autoincrement,
+    name    varchar(45),
+    count   int default 0
+);
+
+
---
+++ docs/Database/tbl_relation-script.sql
@@ -0,0 +1,12 @@
+create table tbl_relation
+(
+    part_id    int not null
+        constraint fk_part_id
+            references tbl_part
+            on delete cascade,
+    visibility tinyint,
+    spec       varchar(45),
+    value      varchar(45)
+);
+
+
